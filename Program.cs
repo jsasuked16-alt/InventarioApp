b2edd4431b2629438596726afdb28ef404dc44bd
Juan Diego C. <jsasuked16@gmail.com>
2026-04-28 03:26:44 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// ===============================================
+// SISTEMA DE INVENTARIO - CLASE 1.1
+// Estado: mensaje de bienvenida
+// ===============================================
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Version: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}"); // Sistema operativo que estamos usando
+Console.WriteLine($".NET Version: {Environment.Version}"); // Version del framework que estamos usando
+Console.WriteLine();
+Console.WriteLine("Estructura del proyecto");
+Console.WriteLine("Configuración csproj");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("Proximos pasos: agregar argumentos CLI y configuración del repositorio en GIthub");
---
+++ README.md
@@ -0,0 +1,22 @@
+# Sistema de gestión de inventario
+
+Proyecto del curso **Fundamentos de .NET** - Platzi
+
+## Requisitos 
+- .NET 10 SDK
+## Como ejecutar 
+dotnet run 
+
+## Estructura del proyecto
+
+InventarioApp/
+- Program.cs # Punto de entrada
+- InventarioApp.csproj # Configuración
+- .gitignore # archivos ignorados por Git 
+- src/
+
+## Autor
+Juan Diego C.
+
+## Progreso del curso
+- [x] Modulo1: El ecosistema .NET
